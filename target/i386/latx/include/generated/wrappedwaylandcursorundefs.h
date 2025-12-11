9a3bec5c1192b22b007281aff5ce57aca665614b
zqz <2264460073@qq.com>
2025-12-11 09:36:46 +0800

    KZT, feat: add Wayland platform support for weston-simple-egl
    Signed-off-by: zqz <2264460073@qq.com>

---
+++ target/i386/latx/include/generated/wrappedwaylandcursorundefs.h
@@ -0,0 +1,5 @@
+#ifndef __wrappedwaylandcursorUNDEFS_H_
+#define __wrappedwaylandcursorUNDEFS_H_
+
+
+#endif // __wrappedwaylandcursorUNDEFS_H_
---
+++ target/i386/latx/include/generated/wrappedwaylandegldefs.h
@@ -0,0 +1,5 @@
+#ifndef __wrappedwaylandeglDEFS_H_
+#define __wrappedwaylandeglDEFS_H_
+
+
+#endif // __wrappedwaylandeglDEFS_H_
