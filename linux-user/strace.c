442501a5c002995d9972ca6a6c4a90ab9e2a2e72
zqz <OrzZzznn@gmail.com>
2026-09-24 14:16:54 +0800

    LATX, fix: Implement guest faccessat2
    Signed-off-by: zqz <OrzZzznn@gmail.com>

--- linux-user/strace.c
+++ linux-user/strace.c
@@ -1938,7 +1938,7 @@ print_execv(void *cpu_env, const struct syscallname *name,
 }
 #endif
 
-#ifdef TARGET_NR_faccessat
+#if defined(TARGET_NR_faccessat) || defined(TARGET_NR_faccessat2)
 static void
 print_faccessat(void *cpu_env, const struct syscallname *name,
                 abi_long arg0, abi_long arg1, abi_long arg2,
